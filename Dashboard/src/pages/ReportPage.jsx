770b139c206517e77849cc70444d4f072027977c
Sungeun Yu <suyou8022@naver.com>
2026-06-09 09:20:58 +0900

    Fix ReportPage API_URL to use VITE_API_URL env var

--- Dashboard/src/pages/ReportPage.jsx
+++ Dashboard/src/pages/ReportPage.jsx
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from 'react'
 import ReactMarkdown from 'react-markdown'
 import './ReportPage.css'
 
-const API_URL = ''
+const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
 
 const INJECT_SCRIPT = `
 <script>
